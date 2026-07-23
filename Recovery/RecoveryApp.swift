d93dad81155112f36fd89347caebcc2cbddf27d0
Ashour Merza <99007576+Ashour21013@users.noreply.github.com>
2026-07-23 13:50:14 +0200

    Initial Commit

---
+++ Recovery/RecoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoveryApp.swift
+//  Recovery
+//
+//  Created by Merza Ashour 6491 ED on 23.07.26.
+//
+
+import SwiftUI
+
+@main
+struct RecoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
